cf60b6804d7217e97f05a5debf4c1a611da1c90e
wyg <1984025877@qq.com>
2018-05-16 17:51:48 +0800

    提交图片

---
+++ public/static/index/js/common.js
@@ -0,0 +1,60 @@
+        var dtLocal = {"passText":"\u67e5\u770b\u8fd9\u4e2a\u52a0\u5bc6\u8d44\u8baf\uff0c\u8bf7\u5728\u4e0b\u9762\u8f93\u5165\u5bc6\u7801\uff1a","moreButtonAllLoadedText":"\u5168\u90e8\u5df2\u52a0\u8f7d","postID":"11965","ajaxurl":"http:\/\/5a4ed3e611bcf.t73.qifeiye.com\/admin\/admin-ajax.php","contactNonce":"0d991525e8","ajaxNonce":"2134fe5221","pageData":{"type":"page","template":"page","layout":null},"themeSettings":{"smoothScroll":"on"}};
+		var thickboxL10n = {"next":"\u4e0b\u4e00\u5f20 >","prev":"< \u4e0a\u4e00\u5f20","image":"\u56fe\u7247","of":"\/","close":"\u5173\u95ed","noiframes":"\u8fd9\u4e2a\u529f\u80fd\u9700\u8981iframe\u7684\u652f\u6301\u3002\u60a8\u53ef\u80fd\u7981\u6b62\u4e86iframe\u7684\u663e\u793a\uff0c\u6216\u60a8\u7684\u6d4f\u89c8\u5668\u4e0d\u652f\u6301\u6b64\u529f\u80fd\u3002","loadingAnimation":"\/\/fast.qifeiye.com\/FeiEditor\/bitSite\/images\/preloader.gif"};
+		dtGlobals.logoEnabled = 1;;
+		dtGlobals.logoURL = '/static/index/imgs/logo1.png';
+		dtGlobals.logoW = '482';
+		dtGlobals.logoH = '126';
+		dtGlobals.qfyname = '西安一笔一画';
+		smartMenu = 1;
+		document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';
+		dtGlobals.gallery_bgcolor = 'rgba(51,51,51,1)';
+		dtGlobals.gallery_showthumbs = '0';
+		dtGlobals.gallery_style = '';
+		dtGlobals.gallery_autoplay = '0';
+		dtGlobals.gallery_playspeed = '3';
+		dtGlobals.gallery_imagesize = '100';
+		dtGlobals.gallery_stopbutton = '';
+		dtGlobals.gallery_thumbsposition = '';
+		dtGlobals.gallery_tcolor = '#fff';
+		dtGlobals.gallery_tsize = '16';
+		dtGlobals.gallery_dcolor = '#fff';
+		dtGlobals.gallery_dsize = '14';
+		dtGlobals.gallery_tfamily = '';
+		dtGlobals.gallery_dfamily = '';
+		dtGlobals.gallery_blankclose = '0';
+		dtGlobals.fm_showstyle = '';
+		dtGlobals.fm_showspeed = '';
+
+
+		/***********分享按钮************/
+        window._bd_share_config={
+            "common":{
+                "bdSnsKey":{},
+                "bdText":"",
+                "bdMini":"2",
+                "bdMiniList":false,
+                "bdPic":"",
+                "bdSize":"16",
+                //分享摘要
+                'bdDesc':'目前支持自定义qq空间，开心网的摘要内容',
+                'render':false
+            },
+            "share":{
+
+            },
+            "image":{
+                /*要分享的图片  加上一个data-tag="imageSharePreview" 不分享的不加该属性*/
+                "tag" : "imageSharePreview",
+                viewType : 'list',
+                viewPos : 'top',
+                viewColor : 'black',
+                viewSize : '16',
+                "viewList":["qzone","tsina","tqq","renren","weixin"],
+                "viewText":"分享到："
+            },
+            "selectShare":{
+                "bdContainerClass":null,
+                "bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]
+            }
+        };
+        with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
