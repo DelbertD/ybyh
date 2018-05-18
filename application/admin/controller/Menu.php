f58a4b04e34e1a91a0516a3e565bddb93020181f
wyg <1984025877@qq.com>
2018-05-18 09:53:13 +0800

    修改后台

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -16,7 +16,42 @@ use think\Db;
 class Menu extends Base
 {
     public function lists(){
-        $db = Db::name('pmenu');
+        $db = Db::name('pmenu')
+            ->orderRaw('sort = 0,sort');
         return parent::_list($db, 10);
+    }
+
+    public function edit($id = 0){
+        if (request()->post()){
+            $post = request()->post();
+            $res = Db::name('pmenu')->update($post);
+            if ($res){
+                $this->success('编辑成功');
+            }else{
+                $this->error('编辑失败');
+            }
+        }else{
+            $menu = Db::name('pmenu')->find($id);
+            $this->assign([
+                'menu' => $menu
+            ]);
+            return $this->fetch();
+        }
+    }
+
+
+    //更改节点排序值
+    public function sort($id = 0, $val = 0){
+        $ret = [
+            'suc' => 0
+        ];
+        $res = Db::name('pmenu')->update([
+            'id'   => $id,
+            'sort' => $val
+        ]);
+        if (!$res){
+            $ret['suc'] = 1;
+        }
+        return $ret;
     }
 }
